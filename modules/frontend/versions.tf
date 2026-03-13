d6bd2adb90dbe74e204f9c1c4dcd2564d00c3917
Håkon Eriksen Drange <haakondrange@gmail.com>
2026-03-13 10:47:53 +0100

    chore: tighten child module provider version constraints to match root

--- modules/frontend/versions.tf
+++ modules/frontend/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 6.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0"
+      version = "~> 3.1"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.0"
+      version = "~> 3.1"
     }
   }
 }
